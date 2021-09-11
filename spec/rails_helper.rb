c70caf0670fc89daee29f669b965d7ef4f04883e
kate gleeson <kate.gleeson@thoughtbot.com>
2021-09-11 19:54:31 +0100

    Initialise RSpec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+
+if Rails.env.production?
+  abort('The Rails environment is running in production mode!')
+end
+
+require 'rspec/rails'
+
+RSpec.configure do |config|
+  config.use_active_record = false
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
