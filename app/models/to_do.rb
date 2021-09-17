b5fd05197c4a2e5c7670e9bb10394edc36d91a0f
kate gleeson <kate.gleeson@thoughtbot.com>
2021-09-17 18:21:11 +0100

    Add state to to_dos

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class ToDo < ApplicationRecord
+  enum state: [:active, :complete, :archived]
   validates :task, presence: true
+  validates :state, presence: true
 end
--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -19,5 +19,21 @@ describe ToDo do
       to_do = build(:to_do, task: 'Post letters')
       expect(to_do.valid?).to eq(true)
     end
+
+    it 'is false if state is not present' do
+      to_do = build(:to_do, state: nil)
+      expect(to_do.valid?).to eq(false)
+
+      expect(
+        to_do.errors.messages[:state]
+      ).to contain_exactly(
+        I18n.t('errors.messages.blank')
+      )
+    end
+
+    it 'is true if state is present' do
+      to_do = build(:to_do, state: ToDo.states[:active])
+      expect(to_do.valid?).to eq(true)
+    end
   end
 end
