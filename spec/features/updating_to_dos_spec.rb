205a7c99ca5eada8c9fec8dbe5ff85daa51772ab
kate gleeson <kate.gleeson@thoughtbot.com>
2021-09-19 21:21:34 +0100

    Add urgent state to to-dos

--- spec/features/updating_to_dos_spec.rb
+++ spec/features/updating_to_dos_spec.rb
@@ -18,6 +18,21 @@ feature 'updaing to-dos', js: true do
     expect(page).to have_selector('span', text: to_do.task, class: 'done')
   end
 
+  scenario 'user updates to-do state to urgent' do
+    user = create(:user)
+    list = create(:list, user: user)
+    to_do = create(:to_do, list: list, state: ToDo.states[:to_do])
+
+    log_in(user)
+    visit(list_to_dos_path(list))
+
+    within("#edit_to_do_#{to_do.id}") do
+      select(I18n.t('to_do.states.urgent'), from: 'to_do_state')
+    end
+
+    expect(page).to have_selector('span', text: to_do.task, class: 'urgent')
+  end
+
   scenario 'user updates to-do state to archived' do
     user = create(:user)
     list = create(:list, user: user)
