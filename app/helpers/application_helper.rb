2ca74ff5012dee4325db5a1a87f43d72a4460046
kate gleeson <kate.gleeson@thoughtbot.com>
2021-09-20 18:12:16 +0100

    Only allow done to-dos to be archived

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def to_do_state_options
+  def state_options(to_do)
     ToDo.states.keys.map do |key|
-      [I18n.t("to_do.states.#{key}"), key]
+      [
+        I18n.t("to_do.states.#{key}"),
+        key,
+        { disabled: (!to_do.done? && key == 'archived') }
+      ]
     end
   end
 end
--- spec/features/updating_to_dos_spec.rb
+++ spec/features/updating_to_dos_spec.rb
@@ -3,21 +3,6 @@
 require 'rails_helper'
 
 feature 'updaing to-dos', js: true do
-  scenario 'user updates to-do state to complete' do
-    user = create(:user)
-    list = create(:list, user: user)
-    to_do = create(:to_do, list: list, state: ToDo.states[:to_do])
-
-    log_in(user)
-    visit(list_to_dos_path(list))
-
-    within("#edit_to_do_#{to_do.id}") do
-      select(I18n.t('to_do.states.done'), from: 'to_do_state')
-    end
-
-    expect(page).to have_selector('span', text: to_do.task, class: 'done')
-  end
-
   scenario 'user updates to-do state to urgent' do
     user = create(:user)
     list = create(:list, user: user)
@@ -33,13 +18,23 @@ feature 'updaing to-dos', js: true do
     expect(page).to have_selector('span', text: to_do.task, class: 'urgent')
   end
 
-  scenario 'user updates to-do state to archived' do
+  scenario 'user updates to-do state to archived and then done' do
     user = create(:user)
     list = create(:list, user: user)
     to_do = create(:to_do, list: list, state: ToDo.states[:to_do])
 
     log_in(user)
     visit(list_to_dos_path(list))
+
+    expect(
+      find('option', text: I18n.t('to_do.states.archived'))
+    ).to be_disabled
+
+    within("#edit_to_do_#{to_do.id}") do
+      select(I18n.t('to_do.states.done'), from: 'to_do_state')
+    end
+
+    expect(page).to have_selector('span', text: to_do.task, class: 'done')
 
     within("#edit_to_do_#{to_do.id}") do
       select(I18n.t('to_do.states.archived'), from: 'to_do_state')
